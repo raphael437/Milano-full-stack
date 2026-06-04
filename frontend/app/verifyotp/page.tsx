3990c740c78d3a2f1ecdf4a16b4eda9f498f4406
raphael437 <123374915+raphael437@users.noreply.github.com>
2026-06-04 22:39:19 +0300

    Update page.tsx

--- frontend/app/verifyotp/page.tsx
+++ frontend/app/verifyotp/page.tsx
@@ -1,5 +1,13 @@
+import { Suspense } from "react";
+
 import VerifyOtpPage from "@/components/verifyOtp";
 
 export default function Page(){
-    return <VerifyOtpPage/>
+     return (
+    <Suspense fallback={<div>Loading...</div>}>
+       <VerifyOtpPage/>
+    </Suspense>
+  );
+
+        
 }
