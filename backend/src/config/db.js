a433aaa82016f121e5df0ea6832b1d840cc58086
raphael437 <123374915+raphael437@users.noreply.github.com>
2026-06-04 17:50:02 +0300

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,30 +1,28 @@
-const Sequelize = require('sequelize');
+const { Sequelize } = require('sequelize');
 const dotenv = require('dotenv');
+
 dotenv.config({ path: './config.env' });
 
-let sequelize;
-
-function getSequelize() {
-  if (!sequelize) {
-    sequelize = new Sequelize(
-      process.env.DB_NAME,
-      process.env.DB_USER,
-      process.env.DB_PASSWORD,
-      {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: 'mysql',
-        logging: false,
-        dialectOptions: {
-          ssl: {
-            rejectUnauthorized: false,
-          },
-        },
-      }
-    );
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+    pool: {
+      max: 1,
+      min: 0,
+      idle: 10000,
+    },
   }
+);
 
-  return sequelize;
-}
-
-module.exports = getSequelize();
+module.exports = sequelize;
